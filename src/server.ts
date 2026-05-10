6910f0c29a0bc3258dda111331dec9c463ee4490
sun4501 <88189687+sonjame@users.noreply.github.com>
2026-05-11 02:43:03 +0900

    Refactor server.ts to improve code formatting

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,10 @@
 (BigInt.prototype as any).toJSON = function () {
-  return this.toString();
+    return this.toString();
 };
 
 import { app } from "./app";
 import { env } from "./config/env";
-import pushRoutes from "./routes/push";
-
-app.use("/api", pushRoutes);
 
 app.listen(env.PORT, () => {
-  console.log(`[server] listening on :${env.PORT}`);
+    console.log(`[server] listening on :${env.PORT}`);
 });
